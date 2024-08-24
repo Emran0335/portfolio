a4d9f360e3c1b0301cfff2580e55467afb14a994
Emran Hossain <emranhossain0335@gmail.com>
2024-08-24 11:22:18 +0600

    information is added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <p className='text-sm py-2 text-center'>
-      &copy; 2023 emrangipsy All rights reserved.
+      &copy; 2021 emrangipsy All rights reserved.
     </p>
   )
 }
